1d5643a67abf93b5461b2f693bc38207fceeabd4
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-11-15 12:22:26 +0100

    finish install script and update ignores

--- build/install.sh
+++ build/install.sh
@@ -5,7 +5,7 @@ set -e
 OS=$(go env GOOS)
 ARCH=$(go env GOARCH)
 ROOT_DIR="$(cd $(dirname "${BASH_SOURCE}")/.. && pwd -P)"
-PLUGIN="terraform.local.com/pluralsh/plural"
+PLUGIN="registry.terraform.io/pluralsh/plural"
 PLUGIN_NAME="terraform-provider-$(basename "${PLUGIN}")"
 PLUGIN_LOCATION="${ROOT_DIR}/build/${PLUGIN_NAME}"
 VERSION=0.0.1
