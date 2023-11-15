d51af60a89115f920af93f371625592c8647e715
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-11-15 12:16:49 +0100

    simplify install script

--- build/install.sh
+++ build/install.sh
@@ -5,13 +5,12 @@ set -e
 OS=$(go env GOOS)
 ARCH=$(go env GOARCH)
 ROOT_DIR="$(cd $(dirname "${BASH_SOURCE}")/.. && pwd -P)"
-PLUGINS_DIR="${HOME}/.terraform.d/plugins"
 PLUGIN="terraform.local.com/pluralsh/plural"
 PLUGIN_NAME="terraform-provider-$(basename "${PLUGIN}")"
 PLUGIN_LOCATION="${ROOT_DIR}/build/${PLUGIN_NAME}"
 VERSION=0.0.1
 FILENAME="${PLUGIN_NAME}_v${VERSION}-${OS}-${ARCH}"
-DEST_PATH="${PLUGINS_DIR}/${PLUGIN}/${VERSION}/${OS}_${ARCH}/${FILENAME}"
+DEST_PATH="${HOME}/.terraform.d/plugins/${PLUGIN}/${VERSION}/${OS}_${ARCH}/${FILENAME}"
 
 mkdir -p "$(dirname "${DEST_PATH}")"
 cp "${PLUGIN_LOCATION}" "${DEST_PATH}"
