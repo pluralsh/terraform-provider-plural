bf163d71190ed849a1f5b6183bea41eb67de4be9
Sebastian Florek <sebastian@plural.sh>
2023-11-16 16:03:45 +0100

    clean up generic env defaulter

--- internal/defaults/env.go
+++ internal/defaults/env.go
@@ -9,27 +9,36 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
-func Env[T string | bool](envVar string, defaultValue T) EnvDefaultValue[T] {
-	return EnvDefaultValue[T]{
+type EnvDefaultValue interface {
+	defaults.String
+	defaults.Bool
+}
+
+type defaultable interface {
+	string | bool
+}
+
+func Env[T defaultable](envVar string, defaultValue T) EnvDefaultValue {
+	return envDefaultValue[T]{
 		envVar:       envVar,
 		defaultValue: defaultValue,
 	}
 }
 
-type EnvDefaultValue[T string | bool] struct {
+type envDefaultValue[T defaultable] struct {
 	envVar       string
 	defaultValue T
 }
 
-func (d EnvDefaultValue[_]) Description(_ context.Context) string {
+func (d envDefaultValue[_]) Description(_ context.Context) string {
 	return fmt.Sprintf("If value is not configured, defaults to a representation of the provided env variable")
 }
 
-func (d EnvDefaultValue[_]) MarkdownDescription(_ context.Context) string {
+func (d envDefaultValue[_]) MarkdownDescription(_ context.Context) string {
 	return fmt.Sprintf("If value is not configured, defaults to a representation of the provided env variable")
 }
 
-func (d EnvDefaultValue[T]) DefaultString(_ context.Context, _ defaults.StringRequest, resp *defaults.StringResponse) {
+func (d envDefaultValue[T]) DefaultString(_ context.Context, _ defaults.StringRequest, resp *defaults.StringResponse) {
 	value := interface{}(d.defaultValue)
 	if v := os.Getenv(d.envVar); len(v) > 0 {
 		value = v
@@ -38,7 +47,7 @@ func (d EnvDefaultValue[T]) DefaultString(_ context.Context, _ defaults.StringRe
 	resp.PlanValue = types.StringValue(value.(string))
 }
 
-func (d EnvDefaultValue[T]) DefaultBool(_ context.Context, _ defaults.BoolRequest, resp *defaults.BoolResponse) {
+func (d envDefaultValue[T]) DefaultBool(_ context.Context, _ defaults.BoolRequest, resp *defaults.BoolResponse) {
 	value := interface{}(d.defaultValue)
 	if v := os.Getenv(d.envVar); len(v) > 0 {
 		value = v == "true"
