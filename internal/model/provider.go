c34646832e1076a37228c7ad7ea1278bde6e49d8
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-11-21 15:02:38 +0100

    extend provider model

--- internal/model/provider.go
+++ internal/model/provider.go
@@ -11,10 +11,23 @@ type Provider struct {
 }
 
 type ProviderCloudSettings struct {
-	AWS ProviderCloudSettingsAWS `tfsdk:"aws"`
+	AWS   ProviderCloudSettingsAWS   `tfsdk:"aws"`
+	Azure ProviderCloudSettingsAzure `tfsdk:"azure"`
+	GCP   ProviderCloudSettingsGCP   `tfsdk:"gcp"`
 }
 
 type ProviderCloudSettingsAWS struct {
 	AccessKeyId     types.String `tfsdk:"access_key_id"`
 	SecretAccessKey types.String `tfsdk:"secret_access_key"`
+}
+
+type ProviderCloudSettingsAzure struct {
+	SubscriptionId types.String `tfsdk:"subscription_id"`
+	TenantId       types.String `tfsdk:"tenant_id"`
+	ClientId       types.String `tfsdk:"client_id"`
+	ClientSecret   types.String `tfsdk:"client_secret"`
+}
+
+type ProviderCloudSettingsGCP struct {
+	Credentials types.String `tfsdk:"credentials"`
 }
