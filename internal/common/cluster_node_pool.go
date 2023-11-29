0d828d9d5c2f991c29751ae7f634f894d7524b6c
Marcin Maciaszczyk <marcin9yk@icloud.com>
2023-11-29 13:18:37 +0100

    add taints attribute

--- internal/common/cluster_node_pool.go
+++ internal/common/cluster_node_pool.go
@@ -14,7 +14,7 @@ type ClusterNodePool struct {
 	MaxSize       types.Int64            `tfsdk:"max_size"`
 	InstanceType  types.String           `tfsdk:"instance_type"`
 	Labels        types.Map              `tfsdk:"labels"`
-	Taints        types.List             `tfsdk:"taints"`
+	Taints        []NodePoolTaint        `tfsdk:"taints"`
 	CloudSettings *NodePoolCloudSettings `tfsdk:"cloud_settings"`
 }
 
@@ -23,6 +23,23 @@ func (c *ClusterNodePool) LabelsAttribute(ctx context.Context, d diag.Diagnostic
 	d.Append(c.Labels.ElementsAs(ctx, &elements, false)...)
 
 	return ToAttributesMap(elements)
+}
+
+func (c *ClusterNodePool) TaintsAttribute(ctx context.Context, d diag.Diagnostics) []*console.TaintAttributes {
+	result := make([]*console.TaintAttributes, 0, len(c.Taints))
+	for _, np := range c.Taints {
+		result = append(result, &console.TaintAttributes{
+			Key:    np.Key.ValueString(),
+			Value:  np.Value.ValueString(),
+			Effect: np.Effect.ValueString(),
+		})
+	}
+}
+
+type NodePoolTaint struct {
+	Key    types.String `tfsdk:"key"`
+	Value  types.String `tfsdk:"value"`
+	Effect types.String `tfsdk:"effect"`
 }
 
 type NodePoolCloudSettings struct {
