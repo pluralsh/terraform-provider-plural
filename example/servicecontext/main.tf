acaa5a0ce2c874e94626a03c4c615dd36a6d00b2
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-02-06 15:29:26 +0100

    add service context example

---
+++ example/servicecontext/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    plural = {
+      source  = "pluralsh/plural"
+      version = "0.0.1"
+    }
+  }
+}
+
+provider "plural" {
+  use_cli = true
+}
+
+resource "plural_service_context" "service_context" {
+  name           = "service-context-tf"
+  configuration = {
+    "env" = "prod"
+    "test" = "some-value"
+  }
+  secrets = {
+    "test" = "some-secret-value"
+  }
+}
