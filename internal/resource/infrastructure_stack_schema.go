c29636d7b7010304d95ada097e5ab0fc3509d700
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-04-19 16:54:40 +0200

    update schema

--- internal/resource/infrastructure_stack_schema.go
+++ internal/resource/infrastructure_stack_schema.go
@@ -3,6 +3,7 @@ package resource
 import (
 	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -13,23 +14,29 @@ func (r *InfrastructureStackResource) schema() schema.Schema {
 	return schema.Schema{
 		Attributes: map[string]schema.Attribute{
 			"id": schema.StringAttribute{
+				Description:         "Internal identifier of this stack.",
+				MarkdownDescription: "Internal identifier of this stack.",
 				Computed:            true,
-				Description:         "Internal identifier of this infrastructure stack.",
-				MarkdownDescription: "Internal identifier of this infrastructure stack.",
 				PlanModifiers:       []planmodifier.String{stringplanmodifier.UseStateForUnknown()},
 			},
 			"name": schema.StringAttribute{
+				Description:         "Human-readable name of this stack.",
+				MarkdownDescription: "Human-readable name of this stack.",
 				Required:            true,
-				Description:         "Human-readable name of this infrastructure stack.",
-				MarkdownDescription: "Human-readable name of this infrastructure stack.",
 				PlanModifiers:       []planmodifier.String{stringplanmodifier.UseStateForUnknown()},
 			},
 			"type": schema.StringAttribute{
-				Required:            true,
 				Description:         "A type for the stack, specifies the tool to use to apply it.",
 				MarkdownDescription: "A type for the stack, specifies the tool to use to apply it.",
+				Required:            true,
 				PlanModifiers:       []planmodifier.String{stringplanmodifier.UseStateForUnknown()},
 				Validators:          []validator.String{stringvalidator.OneOf(string(gqlclient.StackTypeAnsible), string(gqlclient.StackTypeTerraform))},
+			},
+			"approval": schema.BoolAttribute{
+				Description:         "Determines whether to require approval.",
+				MarkdownDescription: "Determines whether to require approval.",
+				Optional:            true,
+				Default:             booldefault.StaticBool(false),
 			},
 		},
 	}
