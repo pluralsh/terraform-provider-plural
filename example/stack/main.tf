f6d88354821b45bcb2f3c09fa3be0fef880e2664
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-04-23 12:48:31 +0200

    update examples

--- example/stack/main.tf
+++ example/stack/main.tf
@@ -20,41 +20,39 @@ data "plural_git_repository" "repository" {
 }
 
 resource "random_string" "random" {
-  length = 5
-  upper = false
+  length  = 5
+  upper   = false
   special = false
 }
 
 # TODO: Test deletion.
-resource "plural_infrastructure_stack" "stack" {
-  name = "stack-tf-${random_string.random.result}"
-  type = "TERRAFORM"
-  approval = true
+resource "plural_infrastructure_stack" "stack-full" {
+  name       = "stack-tf-full-${random_string.random.result}"
+  type       = "TERRAFORM"
+  approval   = true
   cluster_id = data.plural_cluster.cluster.id
   repository = {
-    id = data.plural_git_repository.repository.id
-    ref = "main"
+    id     = data.plural_git_repository.repository.id
+    ref    = "main"
     folder = "terraform"
   }
   configuration = {
-    image = "hashicorp/terraform:1.8.1"
+    image   = "hashicorp/terraform:1.8.1"
     version = "1.8.1"
   }
   files = {
-    # TODO: Test it.
-    # "test.yml": "value: 123"
+    "test.yml" : "value: 123"
   }
   environment = [
-    # TODO: Test it.
-    # {
-    #   name = "USERNAME"
-    #   value = "joe"
-    # },
-    # {
-    #   name = "PASSWORD"
-    #   value = "test"
-    #   secret = true
-    # }
+    {
+      name  = "USERNAME"
+      value = "joe"
+    },
+    {
+      name   = "PASSWORD"
+      value  = "test"
+      secret = true
+    }
   ]
   job_spec = {
     namespace = "default"
@@ -62,24 +60,102 @@ resource "plural_infrastructure_stack" "stack" {
       test = "123"
     }
     service_account = "default"
-    containers = [{
-      image = "perl:5.34.0"
-      args = ["perl",  "-Mbignum=bpi", "-wle", "print bpi(2000)"]
-      # TODO: Test without env and env_from.
-      env = {}
-      env_from = []
-    }]
-#     raw = jsonencode({
-#       containers = [{
-#         name  = "pi"
-#         image = "perl:5.34.0"
-#         command = ["perl",  "-Mbignum=bpi", "-wle", "print bpi(2000)"]
-#       }]
-#       restartPolicy = "Never"
-#     })
+    containers      = [
+      {
+        image    = "perl:5.34.0"
+        args     = ["perl", "-Mbignum=bpi", "-wle", "print bpi(2000)"]
+        env = {}
+        env_from = []
+      }
+    ]
   }
   bindings = {
-     read = []
-     write = []
+    read  = []
+    write = []
+  }
+}
+
+resource "plural_infrastructure_stack" "stack-raw" {
+  name       = "stack-tf-raw-${random_string.random.result}"
+  type       = "TERRAFORM"
+  approval   = true
+  cluster_id = data.plural_cluster.cluster.id
+  repository = {
+    id     = data.plural_git_repository.repository.id
+    ref    = "main"
+    folder = "terraform"
+  }
+  configuration = {
+    image   = "hashicorp/terraform:1.8.1"
+    version = "1.8.1"
+  }
+  files = {
+    "test.yml" : "value: 123"
+  }
+  environment = [
+    {
+      name  = "USERNAME"
+      value = "joe"
+    },
+    {
+      name   = "PASSWORD"
+      value  = "test"
+      secret = true
+    }
+  ]
+  job_spec = {
+    namespace = "default"
+    raw       = jsonencode({
+      containers = [
+        {
+          name    = "pi"
+          image   = "perl:5.34.0"
+          command = ["perl", "-Mbignum=bpi", "-wle", "print bpi(2000)"]
+        }
+      ]
+      restartPolicy = "Never"
+    })
+  }
+  bindings = {
+    read  = []
+    write = []
+  }
+}
+
+resource "plural_infrastructure_stack" "stack-empty" {
+  name       = "stack-tf-empty-${random_string.random.result}"
+  type       = "TERRAFORM"
+  approval   = true
+  cluster_id = data.plural_cluster.cluster.id
+  repository = {
+    id     = data.plural_git_repository.repository.id
+    ref    = "main"
+    folder = "terraform"
+  }
+  configuration = {
+    version = "1.8.1"
+  }
+  files = {}
+  environment = []
+  job_spec = {
+    namespace = "default"
+    labels = {}
+    annotations = {}
+    containers = []
+  }
+  bindings = {}
+}
+
+resource "plural_infrastructure_stack" "stack-minimal" {
+  name       = "stack-tf-minimal-${random_string.random.result}"
+  type       = "TERRAFORM"
+  cluster_id = data.plural_cluster.cluster.id
+  repository = {
+    id     = data.plural_git_repository.repository.id
+    ref    = "main"
+    folder = "terraform"
+  }
+  configuration = {
+    version = "1.8.1"
   }
 }
