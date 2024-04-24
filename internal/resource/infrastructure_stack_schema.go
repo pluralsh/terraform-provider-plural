1317e52bde32930e8fda6c05e910d3f5a83965ef
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-04-24 11:09:17 +0200

    add plan modifiers for labels and annotations

--- internal/resource/infrastructure_stack_schema.go
+++ internal/resource/infrastructure_stack_schema.go
@@ -9,6 +9,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/mapplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/objectplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
@@ -155,6 +156,7 @@ func (r *InfrastructureStackResource) schema() schema.Schema {
 						ElementType:         types.StringType,
 						Optional:            true,
 						Computed:            true,
+						PlanModifiers:       []planmodifier.Map{mapplanmodifier.UseStateForUnknown()},
 						Validators:          []validator.Map{mapvalidator.ConflictsWith(path.MatchRelative().AtParent().AtName("raw"))},
 					},
 					"annotations": schema.MapAttribute{
@@ -163,6 +165,7 @@ func (r *InfrastructureStackResource) schema() schema.Schema {
 						ElementType:         types.StringType,
 						Optional:            true,
 						Computed:            true,
+						PlanModifiers:       []planmodifier.Map{mapplanmodifier.UseStateForUnknown()},
 						Validators:          []validator.Map{mapvalidator.ConflictsWith(path.MatchRelative().AtParent().AtName("raw"))},
 					},
 					"service_account": schema.StringAttribute{
