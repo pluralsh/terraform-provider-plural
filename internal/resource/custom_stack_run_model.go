ccc1f8bb91b8b945a3447f3f8cf9f2295fa4bb88
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-06-04 15:43:37 +0200

    add commands mapping

--- internal/resource/custom_stack_run_model.go
+++ internal/resource/custom_stack_run_model.go
@@ -5,8 +5,10 @@ import (
 
 	"terraform-provider-plural/internal/client"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 	gqlclient "github.com/pluralsh/console-client-go"
 )
 
@@ -36,6 +38,51 @@ func (csr *customStackRun) From(customStackRun *gqlclient.CustomStackRunFragment
 	csr.Name = types.StringValue(customStackRun.Name)
 	csr.Documentation = types.StringPointerValue(customStackRun.Documentation)
 	csr.StackId = types.StringPointerValue(customStackRun.Stack.ID)
-	// TODO Commands
+	csr.Commands = customStackRunCommandsFrom(customStackRun.Commands, csr.Commands, ctx, d)
 	// TODO Configuration
+}
+
+type CustomStackRunCommand struct {
+	Cmd  types.String `tfsdk:"cmd"`
+	Args types.Set    `tfsdk:"args"`
+	Dir  types.String `tfsdk:"dir"`
+}
+
+var CustomStackRunCommandAttrTypes = map[string]attr.Type{
+	"cmd":  types.StringType,
+	"args": types.SetType{ElemType: types.StringType},
+	"dir":  types.StringType,
+}
+
+func customStackRunCommandsFrom(commands []*gqlclient.StackCommandFragment, config types.Set, ctx context.Context, d diag.Diagnostics) types.Set {
+	if len(commands) == 0 {
+		// Rewriting config to state to avoid inconsistent result errors.
+		// This could happen, for example, when sending "nil" to API and "[]" is returned as a result.
+		return config
+	}
+
+	values := make([]attr.Value, len(commands))
+	for i, command := range commands {
+		objValue, diags := types.ObjectValueFrom(ctx, CustomStackRunCommandAttrTypes, CustomStackRunCommand{
+			Cmd:  types.StringValue(command.Cmd),
+			Args: customStackRunCommandArgsFrom(command.Args, ctx, d),
+			Dir:  types.StringPointerValue(command.Dir),
+		})
+		values[i] = objValue
+		d.Append(diags...)
+	}
+
+	setValue, diags := types.SetValue(basetypes.ObjectType{AttrTypes: CustomStackRunCommandAttrTypes}, values)
+	d.Append(diags...)
+	return setValue
+}
+
+func customStackRunCommandArgsFrom(values []*string, ctx context.Context, d diag.Diagnostics) types.Set {
+	if values == nil {
+		return types.SetNull(types.StringType)
+	}
+
+	setValue, diags := types.SetValueFrom(ctx, types.StringType, values)
+	d.Append(diags...)
+	return setValue
 }
