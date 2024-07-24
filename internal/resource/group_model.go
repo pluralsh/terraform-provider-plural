34851f534dd1f0c5cfd549f9bfb44962ccbe7330
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-07-24 14:44:14 +0200

    add group model

---
+++ internal/resource/group_model.go
@@ -0,0 +1,25 @@
+package resource
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	gqlclient "github.com/pluralsh/console-client-go"
+)
+
+type group struct {
+	Id          types.String `tfsdk:"id"`
+	Name        types.String `tfsdk:"name"`
+	Description types.String `tfsdk:"description"`
+}
+
+func (g *group) From(response *gqlclient.GroupFragment) {
+	g.Id = types.StringValue(response.ID)
+	g.Name = types.StringValue(response.Name)
+	g.Name = types.StringPointerValue(response.Description)
+}
+
+func (g *group) Attributes() gqlclient.GroupAttributes {
+	return gqlclient.GroupAttributes{
+		Name:        g.Name.ValueString(),
+		Description: g.Description.ValueStringPointer(),
+	}
+}
