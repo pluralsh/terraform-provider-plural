60c22dbcea93dde3116cb9f02ad32b9acf264ed5
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-09-16 12:55:10 +0200

    fix example

--- example/service/helm/main.tf
+++ example/service/helm/main.tf
@@ -24,14 +24,10 @@ resource "plural_service_deployment" "cd-test" {
     handle = data.plural_cluster.cluster.handle
   }
 
-  # Requires flux-source-controller addon to be installed and flux repo CRD for podinfo to exist
   helm = {
-    chart = "podinfo"
-    repository = {
-      name = "podinfo"
-      namespace = "default"
-    }
-    version = "6.5.3"
+    chart = "grafana"
+    version = "8.x.x"
+    url = "https://grafana.github.io/helm-charts"
   }
 
   # Optional
@@ -39,16 +35,10 @@ resource "plural_service_deployment" "cd-test" {
   docs_path = "doc"
   protect   = false
 
-  configuration = [
-    {
-      name : "host"
-      value : "tf-cd-test.gcp.plural.sh"
-    },
-    {
-      name : "tag"
-      value : "sha-4d01e86"
-    }
-  ]
+  configuration = {
+    "host" = "tf-cd-test.gcp.plural.sh",
+    "tag" = "sha-4d01e86"
+  }
 
   sync_config = {
     namespace_metadata = {
