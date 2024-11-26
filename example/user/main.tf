899322a70c8bbf074172a7d1bb6944f373bcc2f2
Marcin Maciaszczyk <marcin9yk@icloud.com>
2024-11-26 10:50:35 +0100

    add example

--- example/user/main.tf
+++ example/user/main.tf
@@ -17,23 +17,28 @@ data "plural_user" "user" {
   email = "marcin@plural.sh"
 }
 
-data "plural_group" "group" {
-  name = "team"
+resource "plural_user" "user" {
+  name = "Marcin Maciaszczyk"
+  email = "marcin@plural.sh"
 }
 
-resource "plural_group" "test" {
-  name = "test"
-  description = "test group"
-}
-
-resource "plural_rbac" "rbac" {
-  service_id = "624bff88-05e3-45f6-bc3b-44708594e28e"
-  bindings = {
-    read  = [{
-      user_id = data.plural_user.user.id
-    }]
-    write = [{
-      user_id = data.plural_user.user.id
-    }]
-  }
-}
+# data "plural_group" "group" {
+#   name = "team"
+# }
+#
+# resource "plural_group" "test" {
+#   name = "test"
+#   description = "test group"
+# }
+#
+# resource "plural_rbac" "rbac" {
+#   service_id = "624bff88-05e3-45f6-bc3b-44708594e28e"
+#   bindings = {
+#     read  = [{
+#       user_id = data.plural_user.user.id
+#     }]
+#     write = [{
+#       user_id = data.plural_user.user.id
+#     }]
+#   }
+# }
