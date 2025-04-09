5ec4b2d884d28cf8e0d3f7abfca3ed286158935f
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-04-09 11:45:31 +0200

    update example

--- example/oidcprovider/main.tf
+++ example/oidcprovider/main.tf
@@ -11,10 +11,17 @@ provider "plural" {
   use_cli = true
 }
 
+data "plural_user" "user" {
+  email = "marcin@plural.sh"
+}
+
 resource "plural_oidc_provider" "provider" {
   name = "tf-test-provider"
   auth_method = "BASIC"
-  type = "PLURAL"
+  type = "CONSOLE"
   description = "test provider"
   redirect_uris = ["localhost:8000"]
+  bindings = [
+    { user_id = data.plural_user.user.id }
+  ]
 }
