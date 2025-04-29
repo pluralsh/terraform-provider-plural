9a3a9cbaac02bfbc5e3a779d933ac10e961f334b
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-04-29 11:20:19 +0200

    use latest tag in install script

--- build/install.sh
+++ build/install.sh
@@ -8,11 +8,9 @@ ROOT_DIR="$(cd $(dirname "${BASH_SOURCE}")/.. && pwd -P)"
 PLUGIN="registry.terraform.io/pluralsh/plural"
 PLUGIN_NAME="terraform-provider-$(basename "${PLUGIN}")"
 PLUGIN_LOCATION="${ROOT_DIR}/build/${PLUGIN_NAME}"
-VERSION=0.2.1
-DESTINATION="${HOME}/.terraform.d/plugins/${PLUGIN}/${VERSION}/${OS}_${ARCH}/${PLUGIN_NAME}_v${VERSION}-${OS}-${ARCH}"
+VERSION=$(curl -sL https://api.github.com/repos/pluralsh/terraform-provider-plural/tags | jq -r '.[0].name')
+DESTINATION="${HOME}/.terraform.d/plugins/${PLUGIN}/${VERSION//v}/${OS}_${ARCH}/${PLUGIN_NAME}_${VERSION}-${OS}-${ARCH}"
 
 mkdir -p "$(dirname "${DESTINATION}")"
 mv "${PLUGIN_LOCATION}" "${DESTINATION}"
 echo "Installed ${PLUGIN} into ${DESTINATION}"
-
-
