aeebff9ac3c78a3fa0585f32466e5c265e7cf386
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-05-12 15:47:01 +0200

    test new method

--- example/kubeconfig/main.tf
+++ example/kubeconfig/main.tf
@@ -10,36 +10,44 @@ terraform {
 #####################################
 ######### Deprecated method #########
 #####################################
-provider "plural" {
-  alias   = "deprecated"
-  use_cli = true
-}
-
-resource "plural_cluster" "deprecated" {
-  provider = plural.deprecated
-  name     = "byok"
-  protect  = "false"
-  detach   = true
-  kubeconfig = {
-    config_path = pathexpand("~/.kube/config") # This can no longer be sourced from environment variables.
-  }
-}
+# provider "plural" {
+#   alias   = "deprecated"
+#   use_cli = true
+# }
+#
+# resource "plural_cluster" "deprecated" {
+#   provider = plural.deprecated
+#   name     = "byok"
+#   protect  = "false"
+#   detach   = true
+#   kubeconfig = {
+#     config_path = pathexpand("~/.kube/config") # This can no longer be sourced from environment variables.
+#   }
+# }
 
 #####################################
 ############ New method #############
 #####################################
-# provider "plural" {
-#   alias   = "new"
-#   use_cli = true
-#   kubeconfig = {
-#     # Can be sourced from environment variables, export PLURAL_KUBE_CONFIG_PATH to read from local file:
-#     # export PLURAL_KUBE_CONFIG_PATH=$KUBECONFIG
-#   }
-# }
-#
-# resource "plural_cluster" "new" {
-#   provider = plural.new
-#   name     = "byok"
-#   protect  = "false"
-#   detach   = true
-# }
+provider "plural" {
+  alias   = "new"
+  use_cli = true
+  kubeconfig = {
+    # Can be sourced from environment variables, export PLURAL_KUBE_CONFIG_PATH to read from local file:
+    # export PLURAL_KUBE_CONFIG_PATH=$KUBECONFIG
+  }
+}
+
+resource "plural_cluster" "new" {
+  provider = plural.new
+  name     = "byok"
+  protect  = "false"
+  detach   = true
+
+  metadata = jsonencode({
+    test1 = "test"
+    test2 = false
+    test3 = jsonencode({
+      abc = false
+    })
+  })
+}
