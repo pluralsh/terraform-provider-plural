4e2f25ad7072e82b6d84342d8b66a12a7aa8a9ea
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-10-27 12:40:29 +0100

    update example

---
+++ example/cluster/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    plural = {
+      source = "pluralsh/plural"
+      version = "0.2.28"
+    }
+  }
+}
+
+provider "plural" {
+  use_cli = true
+}
+
+resource "plural_cluster" "test" {
+  name = "test-cluster"
+  handle = "test"
+  protect = "false"
+  tags = {
+    "managed-by" = "terraform-provider-plural"
+  }
+}
