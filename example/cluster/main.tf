195701331dc9f84fd286d53622fe5657a3ab62ef
Marcin Maciaszczyk <marcin9yk@icloud.com>
2025-11-17 14:02:22 +0100

    update example

--- example/cluster/main.tf
+++ example/cluster/main.tf
@@ -2,13 +2,17 @@ terraform {
   required_providers {
     plural = {
       source = "pluralsh/plural"
-      version = "0.2.28"
+      version = "0.2.29"
     }
   }
 }
 
 provider "plural" {
   use_cli = true
+  kubeconfig = {
+    # It can be sourced from environment variables instead, i.e.: export PLURAL_KUBE_CONFIG_PATH=$KUBECONFIG
+    config_path = pathexpand("~/.kube/config")
+  }
 }
 
 data "plural_project" "test" {
