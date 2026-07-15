3e318c63435abe1ad2a11f129f574eddb6495232
Marcin Maciaszczyk <marcin9yk@icloud.com>
2026-07-15 11:27:03 +0200

    fix: Fix `plural_cluster` bug (#124)

--- example/cluster/main.tf
+++ example/cluster/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     plural = {
-      source = "pluralsh/plural"
-      version = "0.2.30"
+      source  = "pluralsh/plural"
+      version = "0.2.36"
     }
   }
 }
@@ -10,22 +10,34 @@ terraform {
 provider "plural" {
   use_cli = true
   kubeconfig = {
-    # It can be sourced from environment variables instead, i.e.: export PLURAL_KUBE_CONFIG_PATH=$KUBECONFIG
+    # Can be sourced from environment variables instead:
+    # export PLURAL_KUBE_CONFIG_PATH=$KUBECONFIG
     config_path = pathexpand("~/.kube/config")
   }
 }
 
-data "plural_project" "test" {
-  name = "test"
+data "plural_project" "default" {
+  name = "default"
 }
 
 resource "plural_cluster" "test" {
-  name = "test-cluster"
-  handle = "test"
-  protect = false
-  detach = true
-  project_id = data.plural_project.test.id
+  name       = "test-cluster"
+  handle     = "test"
+  protect    = true
+  detach     = true
+  project_id = data.plural_project.default.id
+  metadata   = jsonencode({ "test-key" = "metadata-update" })
+
   tags = {
-    "managed-by" = "terraform-provider-plural"
+    "managed-by"    = "terraform-provider-plural"
+    "update-marker" = "updated-2"
   }
+}
+
+output "cluster_id" {
+  value = plural_cluster.test.id
+}
+
+output "agent_deployed" {
+  value = plural_cluster.test.agent_deployed
 }
